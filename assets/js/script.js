1a8e3c87c11565b0d2b14e1f79b038505ba9118e
Ajay Pakhare <148714163+ajaypakhare@users.noreply.github.com>
2024-06-18 22:49:40 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('see-all').addEventListener('click', function () {
+        var artistsContainer = document.getElementById('artists-container');
+        if (artistsContainer.style.display === 'none' || artistsContainer.style.display === '') {
+            artistsContainer.style.display = 'block';
+        } else {
+            artistsContainer.style.display = 'none';
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('see-all-1').addEventListener('click', function () {
+        var artistsContainer = document.getElementById('artists-container-1');
+        if (artistsContainer.style.display === 'none' || artistsContainer.style.display === '') {
+            artistsContainer.style.display = 'block';
+        } else {
+            artistsContainer.style.display = 'none';
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('see-all-2').addEventListener('click', function () {
+        var artistsContainer = document.getElementById('artists-container-2');
+        if (artistsContainer.style.display === 'none' || artistsContainer.style.display === '') {
+            artistsContainer.style.display = 'block';
+        } else {
+            artistsContainer.style.display = 'none';
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('see-all-3').addEventListener('click', function () {
+        var artistsContainer = document.getElementById('artists-container-3');
+        if (artistsContainer.style.display === 'none' || artistsContainer.style.display === '') {
+            artistsContainer.style.display = 'block';
+        } else {
+            artistsContainer.style.display = 'none';
+        }
+    });
+});
+
